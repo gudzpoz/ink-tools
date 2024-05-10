506949915600c5393ceeeda8782009fbf91c1696
gudzpoz <gudzpoz@live.com>
2024-05-10 13:50:30 +0800

    Improve annotated context

--- src/cli/annotate.ts
+++ src/cli/annotate.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 
 import { parse as parseCsv } from 'csv-parse/sync';
 import { stringify as stringifyCsv } from 'csv-stringify/sync';
-import { RawSourceMap, SourceMapConsumer } from 'source-map-js';
+import { LineRange, RawSourceMap, SourceMapConsumer } from 'source-map-js';
 
 const files = await fs.readdir('./output/filtered/build/machine-translated');
 
@@ -36,19 +36,35 @@ await Promise.all(files.filter((file) => file.endsWith('.csv'))
       relax_column_count_more: true,
     }) as CsvTranslation[];
     const sourceMap = new SourceMapConsumer(JSON.parse(sourceMapFile) as RawSourceMap);
-    const translationsWithContext: CsvTranslation[] = translations.map((translation, i) => {
-      const lineNumber = i + 2;
-      const positions = sourceMap.allGeneratedPositionsFor({
-        line: lineNumber,
-        column: undefined as never,
-        source: '<input>',
-      });
-      const destLine = positions.map((p) => p.line).reduce((a, b) => {
-        if (a === b) {
-          return a;
+    const lineMapping: Record<number, LineRange> = {};
+    let lastLine = 1;
+    sourceMap.eachMapping((mapping) => {
+      const { generatedLine: inkLine, generatedColumn: inkColumn, originalLine: csvLine } = mapping;
+      if (csvLine !== lastLine) {
+        if (lastLine !== 1) {
+          if (inkLine !== lineMapping[lastLine - 2].line) {
+            throw new Error(`Line ${lastLine - 1} has multiple positions in source map`);
+          }
+          lineMapping[lastLine - 2].lastColumn = inkColumn;
         }
-        throw new Error(`Line ${lineNumber} has multiple positions in source map`);
-      }) - 1;
+        lastLine = csvLine;
+      }
+      if (csvLine === 1) {
+        return;
+      }
+      if (lineMapping[csvLine - 2] === undefined) {
+        lineMapping[csvLine - 2] = {
+          line: inkLine,
+          column: inkColumn,
+          lastColumn: inkColumn,
+        };
+      }
+    });
+    const translationsWithContext: CsvTranslation[] = translations.map((translation, i) => {
+      const position = lineMapping[i];
+      const destLine = position.line - 1;
+      const startColumn = position.column;
+      const endColumn = position.lastColumn;
       const contextBefore = destLines.slice(
         Math.max(0, destLine - 7),
         destLine,
@@ -58,10 +74,11 @@ await Promise.all(files.filter((file) => file.endsWith('.csv'))
         Math.max(0, destLine + 8),
       ).map((s) => `    ${s}`);
       const theLine = `>>> ${destLines[destLine]}`;
+      const underline = `    ${' '.repeat(startColumn)}${'^'.repeat(Math.max(endColumn - startColumn, 1))}`;
       return {
         ...translation,
         comment: `${translation.comment ? `关联：${translation.comment}\n\n` : ''}上下文：\n${
-          [...contextBefore, theLine, ...contextAfter].join('\n').replace(/\n\s+\n/g, '\n')
+          [...contextBefore, theLine, underline, ...contextAfter].join('\n').replace(/\n\s+\n/g, '\n')
         }`,
       };
     });
